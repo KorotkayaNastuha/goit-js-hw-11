b4416b654db7243defee7b6879fe29ceba6fbd5e
KorotkayaNastuha <skiteikina11@gmail.com>
2022-11-11 19:50:50 +0200

    search.img

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+function imageBody() {
+  document.body.classList.remove("background");
+}
