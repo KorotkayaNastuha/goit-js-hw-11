62f470d1c0b288ee369ac7bad64d6ad3dc8c657d
KorotkayaNastuha <skiteikina11@gmail.com>
2022-11-12 22:38:40 +0200

    searchimg

---
+++ src/fetchImages.js
@@ -0,0 +1,18 @@
+export { fetchImages };
+// const key = 31282203 - 7ea8db37884084c288d3f697d;
+    async function fetchImages(value, page) {
+        return await fetch(`https://pixabay.com/api/?key=31282203-7ea8db37884084c288d3f697d&q=${value}&image_type=photo&orientation=horizontal&
+    safesearch=true&page=${page}&per_page=40`)
+            .then(async response => {
+                if (!response.ok) {
+                    if (response.status === 404) {
+                        return;
+                    }
+                    throw new Error(response.status);
+                }
+                return await response.json();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,62 @@
 const axios = require('axios');
+import { fetchImages } from './fetchImages';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const form = document.querySelector(".search-form");
+const input = document.querySelector(".search-form__input");
+const button = document.querySelector(".search-form__btn");
+const gallery = document.querySelector(".gallery");
 
+let page = 1;
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+button.addEventListener('click', event => {
+    event.preventDefault();
+    const value = input.value.trim();
+    if (value !== '') {
+        fetchImages(value, page).then(data => {
+            if (data.hits.length === 0) {
+                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'
+        );
+            } else {
+                imgRender(data.hits);
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`
+                );
+                galleryLightbox.refresh();
+        }
+        })
+    }
+})
+function imgRender(images) {
+    const markup = images.map(image => {
+        console.log(image);
+        return `<div class="photo-card">
+  <a href="${image.webformatURL}">
+  <img class="image" src="${image.largeImageURL}" alt="${image.tags}" loading="lazy"/>
+  </a>
+  <div class="info">
+    <p class="info-item">${image.likes}
+      <b>Likes</b>
+    </p>
+    <p class="info-item">${image.views}
+      <b>Views</b>
+    </p>
+    <p class="info-item">${image.comments}
+      <b>Comments</b>
+    </p>
+    <p class="info-item">${image.downloads}
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+    }).join('');
+    gallery.innerHTML += markup;
+}
 
 // function imageBody() {
 //   document.body.classList.remove("background");
